177f78e551216f2d24b9a940ff8471eee784fdf2
Devanshu Singh <65216265+devanshu06@users.noreply.github.com>
2021-06-23 12:09:51 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import cgi
+import subprocess
+
+print("content-type: text/html")
+print()
+
+
+f = cgi.FieldStorage()
+cmd = f.getvalue("x")
+
+
+o = subprocess.getoutput("sudo " + cmd)
+print("<body>")
+print('<h1 style="color:#000002;" >Output</h1>')
+print("<pre>{}</pre>".format(o))
+print("</body>")
+
+
+#print("Devanshu Singh")
